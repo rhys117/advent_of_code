36bd9eb1336eccda525479444481481063b3a187
Rhys Murray <rhys117@gmail.com>
2021-12-19 21:15:52 +1000

    day 1 finished

--- lib/days/1.rb
+++ lib/days/1.rb
@@ -1,12 +1,29 @@
 module AdventOfCode
   class Day1
-    def self.depths_increased(string)
-      parts = string.split
-      parts.select.with_index do |num, idx|
-        next if idx == 0
-
-        num > parts[idx - 1]
-      end.count
+    def initialize(input)
+      @input_parts = input.split.map(&:to_i)
     end
+
+    def depths_increased(continually_count: 1)
+      result = 0
+      @input_parts.count.times do |count|
+        next if count == 0
+
+        if windowed_sum(index: count, continually_count: continually_count) > windowed_sum(index: count - 1, continually_count: continually_count)
+          result += 1
+        end
+      end
+
+      result
+    end
+
+    private
+
+      def windowed_sum(index:, continually_count:)
+        upto_index = index + continually_count - 1
+        return 0 if @input_parts[upto_index].nil?
+
+        (@input_parts[index..upto_index]).to_a.inject(:+)
+      end
   end
 end
--- spec/lib/days/1_spec.rb
+++ spec/lib/days/1_spec.rb
@@ -4,8 +4,26 @@ RSpec.describe AdventOfCode::Day1 do
   describe '#depths_increased' do
     it 'should return example case correctly' do
       expect(
-        described_class.depths_increased(file_fixture('day_1.txt'))
+        described_class.new(file_fixture('day_1.txt')).depths_increased
       ).to eq(7)
     end
+  end
+
+  it 'should pass with personalized input' do
+    expect(
+      described_class.new(file_fixture('day_1_personalized.txt')).depths_increased
+    ).to eq(1462)
+  end
+
+  it 'should handle continually window' do
+    expect(
+      described_class.new(file_fixture('day_1.txt')).depths_increased(continually_count: 3)
+    ).to eq(5)
+  end
+
+  it 'should handle continually window with personalized input' do
+    expect(
+      described_class.new(file_fixture('day_1_personalized.txt')).depths_increased(continually_count: 3)
+    ).to eq(1497)
   end
 end
