51d5d135f935c2179ee96ec88e126b4c81897d63
Rhys Murray <rhys117@gmail.com>
2021-12-20 23:54:04 +1000

    day 3 finished

--- lib/days/3/binary_diagnostic.rb
+++ lib/days/3/binary_diagnostic.rb
@@ -1,24 +1,37 @@
 class BinaryDiagnostic
-  attr_reader :gamma_rate, :epsilon_rate
+  attr_reader :gamma_rate, :epsilon_rate, :oxygen_generator_rating, :o2_scrubber_rating
 
   def initialize(input_string)
     @split_binary_strings = input_string.split
     @binary_strings_count = @split_binary_strings.count
-    @count_map = determine_count_map
+    @binary_parts_size = @split_binary_strings.first.size
+    @default_count_map = determine_count_map(@split_binary_strings)
     @gamma_rate = determine_gamma
     @epsilon_rate = determine_epilson
+    @oxygen_generator_rating = determine_oxygen_generator_rating
+    @o2_scrubber_rating = determine_o2_scrubber_rating
   end
 
   def gamma_and_epsilon_multiple
     @gamma_rate * @epsilon_rate
   end
 
+  def oxygen_generator_and_o2_scrubber_multiple
+    @oxygen_generator_rating * @o2_scrubber_rating
+  end
+
   private
 
-  def determine_count_map
+  def determine_count_map(parts, position: nil, most_common: true)
     results = Hash.new(0)
-    @split_binary_strings.each do |str|
+    parts.each do |str|
       str.split('').each_with_index { |str_int, index| results[index] += 1 if str_int == '1' }
+    end
+
+    unless position.nil?
+      determined_filter = determine_bit(results[position], most_common: most_common, parts_count: @filtered_parts.count)
+      @filtered_parts = parts.select { |part| part[position] == determined_filter }
+      determine_count_map(@filtered_parts)
     end
 
     results
@@ -26,8 +39,8 @@ class BinaryDiagnostic
 
   def determine_gamma
     str = ''
-    (@split_binary_strings.first.size).times do |position|
-      str << determine_bit(@count_map[position])
+    @binary_parts_size.times do |position|
+      str << determine_bit(@default_count_map[position])
     end
 
     str.to_i(2)
@@ -35,18 +48,42 @@ class BinaryDiagnostic
 
   def determine_epilson
     str = ''
-    (@split_binary_strings.first.size).times do |position|
-      str << determine_bit(@count_map[position], most_common: false)
+    @binary_parts_size.times do |position|
+      str << determine_bit(@default_count_map[position], most_common: false)
     end
 
     str.to_i(2)
   end
 
-  def determine_bit(one_count, most_common: true)
-    one_count_greatest = one_count > (@binary_strings_count / 2)
-    if one_count_greatest && most_common
+  def determine_oxygen_generator_rating
+    @filtered_parts = @split_binary_strings.dup
+    count_map = nil
+    position = 0
+    until @filtered_parts.size == 1
+      count_map = determine_count_map(@filtered_parts, position: position)
+      position += 1
+    end
+
+    @filtered_parts.first.to_i(2)
+  end
+
+  def determine_o2_scrubber_rating
+    @filtered_parts = @split_binary_strings.dup
+    count_map = nil
+    position = 0
+    until @filtered_parts.size == 1
+      count_map = determine_count_map(@filtered_parts, position: position, most_common: false)
+      position += 1
+    end
+
+    @filtered_parts.first.to_i(2)
+  end
+
+  def determine_bit(one_count, most_common: true, parts_count: @binary_strings_count)
+    one_count_greater = one_count >= (parts_count.to_f / 2)
+    if one_count_greater && most_common
       '1'
-    elsif one_count_greatest && !most_common
+    elsif one_count_greater && !most_common
       '0'
     elsif !most_common
       '1'
--- spec/days/3/binary_diagnostic_spec.rb
+++ spec/days/3/binary_diagnostic_spec.rb
@@ -17,6 +17,22 @@ describe BinaryDiagnostic do
     end
   end
 
+  describe 'oxygen_generator_rating' do
+    it 'should return correctly with default input' do
+      expect(
+        described_class.new(file_fixture('day_3.txt')).oxygen_generator_rating
+      ).to eq(23)
+    end
+  end
+
+  describe 'o2_scrubber_rating' do
+    it 'should return correctly with default input' do
+      expect(
+        described_class.new(file_fixture('day_3.txt')).o2_scrubber_rating
+      ).to eq(10)
+    end
+  end
+
   describe 'gamma_and_epsilon_multiple' do
     it 'should return correctly with default input' do
       expect(
@@ -28,6 +44,20 @@ describe BinaryDiagnostic do
       expect(
         described_class.new(file_fixture('day_3_personalized.txt')).gamma_and_epsilon_multiple
       ).to eq(3429254)
+    end
+  end
+
+  describe 'gamma_and_epsilon_multiple' do
+    it 'should return correctly with default input' do
+      expect(
+        described_class.new(file_fixture('day_3.txt')).oxygen_generator_and_o2_scrubber_multiple
+      ).to eq(230)
+    end
+
+    it 'should return correctly with personalized input' do
+      expect(
+        described_class.new(file_fixture('day_3_personalized.txt')).oxygen_generator_and_o2_scrubber_multiple
+      ).to eq(5410338)
     end
   end
 end
